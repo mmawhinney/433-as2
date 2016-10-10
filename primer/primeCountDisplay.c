dfe0a9714b026176d8bc9f84321adeb322291b74
Matheson Mawhinney <mmawhinn@sfu.ca>
2016-10-09 22:06:50 -0700

    add segmented display module

---
+++ primer/primeCountDisplay.c
@@ -0,0 +1,5 @@
+
+
+void* primeCountDisplay(void* args) {
+	
+}
---
+++ primer/primeCountDisplay.h
@@ -0,0 +1,7 @@
+#ifndef _PRIME_COUNT_DISPLAY_H_
+#define _PRIME_COUNT_DISPLAY_H_
+
+
+void* primeCountDisplay_launchThread(void* args);
+
+#endif
